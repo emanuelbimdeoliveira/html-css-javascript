15c27ae0f3e919adba3dea7d92eaeac5c3e9b0a8
EMANUEL DOS SANTOS BIM DE OLIVEIRA <130716978+EmanuelBimDeOliveira@users.noreply.github.com>
2023-09-05 22:07:58 -0300

    delete and check functions

--- todolist/index.js
+++ todolist/index.js
@@ -12,7 +12,7 @@ const addTask = () => {
     let task = {}
 
     const taskContent = textInput.value;
-    const taskChecked = false;
+    const taskChecked = "unchecked";
 
     task.content = taskContent;
     task.checked = taskChecked;
@@ -20,6 +20,8 @@ const addTask = () => {
     arrayOfTasks.push(task);
 
     updateScreen();
+
+    textInput.value = "";
 }
 
 const updateScreen = () => {
@@ -28,15 +30,33 @@ const updateScreen = () => {
     arrayOfTasks.forEach((element, index) => {
         const li = document.createElement("li");
         li.innerHTML = `
-            <input type="checkbox" name="checkbox" id="${index}">
-            <label for="${index}">
+            <input type="checkbox" name="checkbox" id="${index}" ${element.checked}>
+            <label for="${index}" onclick="check(${index})">
                 <p>${element.content}</p>
             </label>
-            <input type="button" value="X">
+            <input type="button" value="X" onclick="deleteTask(${index})"> 
         `;
         unorderedList.appendChild(li);
     });
 }
 
+const check = (index) => {
+    arrayOfTasks[index].checked = arrayOfTasks[index].checked == "checked" ? "unchecked" : "checked";
+    updateScreen();
+}
+
+const deleteTask = (index) => {
+    arrayOfTasks.splice(index, 1);    
+    updateScreen();
+}
+
 
 buttonInput.addEventListener("click", addTask);
+
+window.addEventListener("keypress", (event) => {
+    console.log(event)
+    if (event.key == "Enter") {
+        event.preventDefault();
+        addTask();
+    }
+})
