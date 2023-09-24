3417599fcadac29d37980b2b6d64d4b2777da315
EMANUEL DOS SANTOS BIM DE OLIVEIRA <130716978+EmanuelBimDeOliveira@users.noreply.github.com>
2023-09-24 15:22:38 -0300

    responsive layout, increase content function, undefined problem fixed

--- cityapi/index.js
+++ cityapi/index.js
@@ -1,24 +1,25 @@
 "use strict";
 
 const body = document.querySelector("body");
+let limit = 50;
 
 const data = [
 	{id: "continent", h2Text: "Selecione um continente", inputPlaceholder: "America do Sul", thText: "Nomes dos Países"},
 	{id: "country", h2Text: "Digite a sigla de um país", inputPlaceholder: "Exemplo: Br, Us, Fr, It", inputValue: "br", thText: "Nomes dos estados ou províncias:"},
-	{id: "state", h2Text: "Digite a sigla do estado desejado:", inputPlaceholder: "Exemplo: SP, PR, RJ", inputValue: "sp", thText: "Nomes das cidades:"}
-]
+	{id: "state", h2Text: "Digite a sigla do estado:", inputPlaceholder: "Exemplo: SP, PR, RJ", inputValue: "sp", thText: "Nomes das cidades:"}
+];
 
 for (let i = 0; i < 3; i++) {
 	const section = document.createElement("section");
 	section.id = data[i].id;
 	section.classList.add("section-of-content");
 	section.innerHTML = `
-		<form class="form-control container-lg bg-light my-5">
-			<button type="button" class="btn btn-warning border-dark material-symbols-outlined" onclick="history.back()">arrow_back</button>
-			<h2 class="bg-info text-bg-info text-center">${data[i].h2Text}</h2>
+		<form class="form-control container-lg bg-light mt-5">
+			<button type="button" class="btn btn-warning border-dark mt-3 material-symbols-outlined" onclick="history.back()">arrow_back</button>
+			<h2 class="bg-info text-bg-info text-center mt-3 rounded-2">${data[i].h2Text}</h2>
 			<fieldset class="input-group container-lg d-flex justify-content-center py-5">
-				<input type="text" placeholder="${data[i].inputPlaceholder}" value="${data[i].inputValue}" class="p-2 w-50 input-of-text">
-				<select name="continent-name" id="continent-name" class="select-of-continents w-50">
+				<input type="text" placeholder="${data[i].inputPlaceholder}" value="${data[i].inputValue}" class="p-2 w-75 input-of-text">
+				<select name="continent-name" id="continent-name" class="select-of-continents w-75">
 					<option value="eu">Europa</option>
 					<option value="na">América do Norte</option>
 					<option value="sa" selected>América do Sul</option>
@@ -43,10 +44,10 @@ for (let i = 0; i < 3; i++) {
 			</table>    
 			<div id="image" class="container-lg"></div>
 		</div>
+
 	`;
 	body.appendChild(section);
 }
-
 
 const options = {
 	method: 'GET',
@@ -56,6 +57,12 @@ const options = {
 	}
 };
 
+
+// funções
+const increaseLimit = (index) => {
+	limit += 20;
+	catchResponse(index);
+}
 
 const catchResponse = async (index) => {
 	const inputText = document.querySelectorAll("input[type=text]");
@@ -70,21 +77,20 @@ const catchResponse = async (index) => {
 
 	switch (section[index].id) {
 		case "continent": 
-			url = `https://referential.p.rapidapi.com/v1/country?fields=currency%2Ccurrency_num_code%2Ccurrency_code%2Ccontinent_code%2Ccurrency%2Ciso_a3%2Cdial_code&continent_code=${inputSelect.value}&lang=pt&limit=250`;
-			break
+			url = `https://referential.p.rapidapi.com/v1/country?fields=currency%2Ccurrency_num_code%2Ccurrency_code%2Ccontinent_code%2Ccurrency%2Ciso_a3%2Cdial_code&continent_code=${inputSelect.value}&lang=pt&limit=${limit}`;
+			break;
 		case "country": 
-			url = `https://referential.p.rapidapi.com/v1/state?fields=iso_a2&iso_a2=${inputText[index].value}&lang=pt&limit=250`;
-			break
+			url = `https://referential.p.rapidapi.com/v1/state?fields=iso_a2&iso_a2=${inputText[index].value}&lang=pt&limit=${limit}`;
+			break;
 		case "state": 
-			url = `https://referential.p.rapidapi.com/v1/city?fields=iso_a2%2Cstate_code%2Cstate_hasc%2Ctimezone%2Ctimezone_offset&iso_a2=br&lang=pt&state_code=${inputText[index].value}&limit=250`;
-			break
+			url = `https://referential.p.rapidapi.com/v1/city?fields=iso_a2%2Cstate_code%2Cstate_hasc%2Ctimezone%2Ctimezone_offset&iso_a2=br&lang=pt&state_code=${inputText[index].value}&limit=${limit}`;
+			break;
 	}
-
 
 	try {
 		response = await fetch(url, options);
 	} catch (error) {
-		console.log(error.name)
+		alert("Tente novamente!");
 	}
 
 	if (response.status == 200) {
@@ -96,27 +102,40 @@ const catchResponse = async (index) => {
 		})
 		resultArray.sort()
 
-		console.log(resultArray)
-
-		for (let i = 0; i <= resultArray.length; i += 3) {
+		for (let i = 0; i < resultArray.length; i += 3) {
 			const tableRow = document.createElement("tr");		
 			tableRow.innerHTML = `
 				<td>
-					<p class="w-50 d-inline float-left">- ${resultArray[i]}</p>
+					<p class="w-50 d-inline float-start">${resultArray[i]}</p>
 					<a class="find-place nav-link material-symbols-outlined" href="https://www.google.com.br/maps/place/${resultArray[i]}" target="_blank" title="Localizar no Google Maps">location_on</a>
 				</td>
 				<td>
-					<p class="w-50 d-inline float-left">- ${resultArray[i += 1]}</p>
+					<p class="w-50 d-inline float-start">${resultArray[i += 1]}</p>
 					<a class="find-place nav-link material-symbols-outlined" href="https://www.google.com.br/maps/place/${resultArray[i]}" target="_blank" title="Localizar no Google Maps">location_on</a>
 				</td>
 				<td>
-					<p class="w-50 d-inline float-left">- ${resultArray[i += 1]}</p>
+					<p class="w-50 d-inline float-start">${resultArray[i += 1]}</p>
 					<a class="find-place nav-link material-symbols-outlined" href="https://www.google.com.br/maps/place/${resultArray[i]}" target="_blank" title="Localizar no Google Maps">location_on</a>
 				</td>
 			`;
 			i -= 2;
 			tableBody[index].appendChild(tableRow);
 		}	
+		
+		const lastRow = document.querySelectorAll("tr:last-of-type p");
+		lastRow.forEach((element) => {
+			if (element.textContent == "undefined") {
+				element.parentElement.style.display = "none";
+			}
+		})
+
+		const tableRowIncreaseContent = document.createElement("tr");		
+		tableRowIncreaseContent.innerHTML = `
+			<td class="text-center py-2" colspan="3">
+				<button type="button" class="btn btn-warning text-bg-warning text-center w-50 mx-auto" onclick="increaseLimit(${index})">Mostrar Mais</button>
+			</td>
+		`;
+		tableBody[index].appendChild(tableRowIncreaseContent);
 	} else {
 		const tableRow = document.createElement("tr");		
 		tableRow.innerHTML = `
@@ -125,3 +144,21 @@ const catchResponse = async (index) => {
 		tableBody[index].appendChild(tableRow);
 	}
 }
+
+
+body.addEventListener("keydown", (event) => {
+	if (event.key == "Enter") {
+		event.preventDefault();
+		switch (event.view.location.hash) {
+			case "#continent":
+				catchResponse(0);
+				break;
+			case "#country":
+				catchResponse(1);
+				break;
+			case "#state":
+				catchResponse(2);
+				break;			
+		}		
+	}
+});
